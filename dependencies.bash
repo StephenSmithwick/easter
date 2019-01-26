f3b833dda44fd0920f9b5743094a8bbafd744c4c
StephenSmithwick <Stephen.Smithwick@gmail.com>
2019-01-27 08:38:19 +1100

    uncommenting base dependecy install

--- dependencies.bash
+++ dependencies.bash
@@ -3,10 +3,7 @@
 source "$HOME/easter/utilities.bash"
 
 echo "Checking Easter Dependencies:"
-# deps base title="Base"
-# deps languages title="Languages"
-# deps tools title="Tools"
-# deps platforms title="Platform Tools"
+deps base title="Base"
 # deps private title="Locations (Private)"
 
 for basket in `ls  $EASTER_HOME/baskets/*.yml`; do
